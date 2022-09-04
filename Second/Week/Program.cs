6253e759659f62f97c9ae248ed96029dad4c661d
Olga <princessca.olga@icloud.com>
2022-09-04 14:56:20 +0300

    Домашка

---
+++ Second/Week/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введи цифру, обозначающую день недели: ");
+int day= Convert.ToInt32(Console.ReadLine());
+
+if(day<=5){
+
+Console.WriteLine("рабочий");
+}
+else
+{
+Console.WriteLine("выходной");
+}
+
+Console.ReadLine();
